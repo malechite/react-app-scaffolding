004f1c4137dd518b4a91086acaac04b341251f62
Brandon Dusseau <malechite@gmail.com>
2017-02-23 13:43:36 -0600

    Hello World again

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     render() {
         return (
             <div className={styles.dashboard}>
-                Dashboard
+                Dashboard - Hello World!
                 <Button iconName='refresh' intent={Intent.PRIMARY}>Delete</Button>
             </div>
         );
