2ddacbd9710870ab18051ee86214dabf32485536
Brandon Dusseau <malechite@gmail.com>
2017-03-27 12:32:48 -0500

    Revert "Switched form custom server to webpack-dev-server"
    This reverts commit 6ec9784606b213250b329ac4345f843d23cdd39a.

---
+++ bin/dev-server.js
@@ -0,0 +1,28 @@
+/* eslint no-console:0 */
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    //dev only
+    var webpackConfig = require('../webpack.dev-config');
+    var webpack = require('webpack');
+    var compiler = webpack(webpackConfig);
+
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo: true, publicPath: webpackConfig.output.publicPath
+    }));
+
+    app.use(require('webpack-hot-middleware')(compiler, {
+        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+    }));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../src/index.html'));
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %s...', server.address().port);
+});
---
+++ webpack.dev-config.js
@@ -0,0 +1,95 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        './src/main.js',
+        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true'
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src'),
+            'node_modules'
+        ],
+        alias: {
+            Styles: path.resolve(__dirname, 'src/styles'),
+            Utilities: path.resolve(__dirname, 'src/shared/utilities')
+        }
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: '[name]-[hash].js',
+        chunkFilename: '[name]-[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'react-hmre', 'es2015']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader?modules&localIdentName=[path][name]---[local]---[hash:base64:5]',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use:'url-loader?prefix=font/&limit=5000'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: 'file-loader?name=assets/[name]-[hash].[ext]'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    devtool: 'eval',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function(module) {
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
