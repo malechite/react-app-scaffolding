aa30443614d0b1e792fba1d404815ae8378bd8d5
Brandon Everhart <malechite@gmail.com>
2018-09-07 08:20:24 -0500

    Updating version numbers on mock dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -13,21 +13,21 @@ export default class Dashboard extends Component {
               <Card elevation={Elevation.ONE}>
                 <h5 className="bp3-heading">React</h5>
                 <p>A JavaScript library for building user interfaces.</p>
-                <span className={styles.version}>v15.4.2</span>
+                <span className={styles.version}>v16.4.2</span>
               </Card>
             </Col>
             <Col xs={12} md={4} className={styles.col}>
               <Card elevation={Elevation.ONE}>
                 <h5 className="bp3-heading">Redux</h5>
                 <p>A predictable state container for JavaScript apps.</p>
-                <span className={styles.version}>v3.6.0</span>
+                <span className={styles.version}>v4.0.0</span>
               </Card>
             </Col>
             <Col xs={12} md={4} className={styles.col}>
               <Card elevation={Elevation.ONE}>
                 <h5 className="bp3-heading">Webpack</h5>
                 <p>A module bundler for modern JavaScript applications.</p>
-                <span className={styles.version}>v2.3.1</span>
+                <span className={styles.version}>v4.17.0</span>
               </Card>
             </Col>
           </Row>
@@ -43,14 +43,14 @@ export default class Dashboard extends Component {
               <Card elevation={Elevation.ONE}>
                 <h5 className="bp3-heading">Blueprint</h5>
                 <p>A React-based UI toolkit for the web.</p>
-                <span className={styles.version}>v1.14.0</span>
+                <span className={styles.version}>v3.3.0</span>
               </Card>
             </Col>
             <Col xs={12} md={4} className={styles.col}>
               <Card elevation={Elevation.ONE}>
-                <h5 className="bp3-heading">Yarn</h5>
-                <p>Fast, reliable, and secure dependency management.</p>
-                <span className={styles.version}>v0.23.0</span>
+                <h5 className="bp3-heading">Styled Compnents</h5>
+                <p>Visual primitives for the component age.</p>
+                <span className={styles.version}>v3.2.5</span>
               </Card>
             </Col>
           </Row>
